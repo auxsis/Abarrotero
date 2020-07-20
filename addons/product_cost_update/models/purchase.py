3e8c9a496a932d4be7903c648d013b7f55413ca9
Vladimir Alvarez <vladimir881002@gmail.com>
2020-07-20 17:51:08 +0200

    [FIX]Actualiza la lista de precios de proveedores en los productos

--- addons/product_cost_update/models/purchase.py
+++ addons/product_cost_update/models/purchase.py
@@ -27,3 +27,12 @@ class PurchaseOrder(models.Model):
         for order in order_ids:
             for line in order.order_line:
                 line.product_id.write({"standard_price": line.coste_neto})
+
+    def fix_seller_price(self):
+        order_ids = self.env['purchase.order'].search([('state', '=', 'purchase')], order='id asc')
+        for order in order_ids:
+            for line in order.order_line:
+                seller = line.product_id.seller_ids.filtered(lambda sel: sel.name.id == order.partner_id.id)
+                if seller:
+                    print("Here")
+                seller.write({"price": line.price_unit})
