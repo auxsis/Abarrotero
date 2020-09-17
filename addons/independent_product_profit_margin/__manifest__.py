5063cccefe07f9c70c11c07db0fbf8b276c80e85
jfernandez-890825 <juan.carlos.fdez.89@gmail.com>
2020-09-16 22:21:52 -0400

    Arreglando incoherencias

--- addons/independent_product_profit_margin/__manifest__.py
+++ addons/independent_product_profit_margin/__manifest__.py
@@ -13,7 +13,6 @@
     """,
 
     'author': "Juan Carlos Fernández Hernández",
-    # 'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -23,17 +22,6 @@
 
     # any module necessary for this one to work correctly
     'depends': ['product_profit_margin'],
-
-    # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        # 'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    # 'demo': [
-    #     'demo/demo.xml',
-    # ],
 
     "post_init_hook": "post_init_hook",
     "pre_init_hook": "pre_init_hook",
