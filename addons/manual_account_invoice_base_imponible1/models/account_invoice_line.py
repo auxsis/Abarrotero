b7019b1f006c19d3d142d3bfe4d2b23087e8c3c7
jcfernandez-890825 <juan.carlos.fdez.89@gmail.com>
2020-10-08 23:27:32 -0400

    Se fuerza a que siempre coja el valor del campo "Base Imponible LP1"

--- addons/manual_account_invoice_base_imponible1/models/account_invoice_line.py
+++ addons/manual_account_invoice_base_imponible1/models/account_invoice_line.py
@@ -8,7 +8,8 @@ class AccountInvoiceLine(models.Model):
 
     price_unit = fields.Float(string='Unit Price', compute="_compute_baseimponible1", store=True)
 
-    @api.depends('product_id', 'product_id.base_imponible1')
+    @api.depends('product_id', 'product_id.base_imponible1', 'price_unit')
     def _compute_baseimponible1(self):
         for line in self.filtered(lambda l: not bool(l.origin)):
-            line.price_unit = line.product_id.base_imponible1
+            if line.price_unit != line.product_id.base_imponible1:
+                line.price_unit = line.product_id.base_imponible1
