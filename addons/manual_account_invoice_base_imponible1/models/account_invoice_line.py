6092bc9c2d54596480c55e129930b140ec53d9d9
jcfernandez-890825 <juan.carlos.fdez.89@gmail.com>
2020-10-09 00:26:59 -0400

    Se fuerza a que siempre coja el valor del campo "Base Imponible LP1"

--- addons/manual_account_invoice_base_imponible1/models/account_invoice_line.py
+++ addons/manual_account_invoice_base_imponible1/models/account_invoice_line.py
@@ -1,15 +1,23 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.addons import decimal_precision as dp
 
 
 class AccountInvoiceLine(models.Model):
     _inherit = "account.invoice.line"
 
-    price_unit = fields.Float(string='Unit Price', compute="_compute_baseimponible1", store=True)
+    price_unit = fields.Float(
+        string='Unit Price', compute="_compute_baseimponible1", store=True, digits=dp.get_precision('Product Price')
+    )
 
-    @api.depends('product_id', 'product_id.base_imponible1', 'price_unit')
+    @api.onchange('price_unit')
+    def _set_baseimponible1(self):
+        base_imponible1 = self.product_id.base_imponible1
+        if self.price_unit != base_imponible1:
+            self.price_unit = base_imponible1
+
+    @api.depends('product_id')
     def _compute_baseimponible1(self):
         for line in self.filtered(lambda l: not bool(l.origin)):
-            if line.price_unit != line.product_id.base_imponible1:
-                line.price_unit = line.product_id.base_imponible1
+            line.price_unit = line.product_id.base_imponible1
