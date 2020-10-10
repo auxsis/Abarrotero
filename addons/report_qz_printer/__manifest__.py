a4baa9b0493e4e8bae21fa4a7ac981403e00e325
jcfernandez-890825 <juan.carlos.fdez.89@gmail.com>
2020-10-09 20:49:02 -0400

    Tarea: "Impresión directa" (sin descargar pdf).
    Insertar las funciones necesarias para la impresión con QZ.
    Modificar la función "printReport" para imprimir PDF en base64.
    Obtener la URL del reporte PDF.
    Modificar el PDF de la URL a base64.
    Obtener los nombres de los reportes que van a usar la impresión QZ.

---
+++ addons/report_qz_printer/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "report_qz_printer",
+
+    'summary': """
+        Imprime documentos sin tener que descargar el PDF, usando QZ.
+    """,
+
+    'description': """
+        Imprime documentos sin tener que descargar el PDF, usando QZ.
+    """,
+
+    'author': "Odoo Experts MX",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Printer',
+    'version': '12.0.1.0.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'views/res_company_view.xml',
+        'views/template.xml',
+    ],
+}
