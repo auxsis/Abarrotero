893785f0daec1fd3ea8001e4ad264a8c3fc84b59
jorgeluislr <jorgeluislr93@gmail.com>
2020-10-13 11:50:26 -0400

    [FIX]Sobrescribiendo el método _name_search del modelo catalogos.claveprodserv.

--- addons/catalogos_cfdi/models/clave_prodserv.py
+++ addons/catalogos_cfdi/models/clave_prodserv.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class ClaveProdServ(models.Model):
@@ -9,3 +9,10 @@ class ClaveProdServ(models.Model):
 
     c_claveprodserv = fields.Char(string='Clave de Producto o servicio')
     descripcion = fields.Char(string='Descripción')
+
+    @api.model
+    def _name_search(self, name='', args=None, operator='ilike', limit=100):
+        if args is None:
+            args = []
+        domain = args + ['|', ('c_claveprodserv', operator, name), ('descripcion', operator, name)]
+        return super(ClaveProdServ, self).search(domain, limit=limit).name_get()
